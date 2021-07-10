90a75cc5a2252112caecde24cbfa1097b2fea15e
Thomas Guettler <guettliml@thomas-guettler.de>
2021-07-10 07:00:23 +0200

    initial Thing model.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # djntra
-Djntra: Django based Intranet
+
+# Install
+
+```
+python3 -m venv djntra-env
+cd djntra-env/
+. bin/activate
+pip install -U pip wheel
+pip install -e git+ssh://git@github.com/guettli/djntra.git#egg=djntra
+cp src/djntra/.env.example src/djntra/.env
+echo '. $VIRTUAL_ENV/src/djntra/.env' >> bin/activate
+echo 'export $(cut -d= -f1 $VIRTUAL_ENV/src/djntra/.env)' >> bin/activate
+
+. bin/activate
+
+# You need to have PostgreSQL installed
+# Create user "djntra" with password "djntra":
+sudo runuser -u postgres -- createuser -s -P djntra
+
+createdb $PGDATABASE
+manage.py migrate
+```
+
+The migration create a user "anonymous" (for not authorized users) and "admin" (with password "admin").
+
+# Naming convention
+
+See: https://github.com/guettli/django-htmx-fun
+
+# Guidelines
+
+See: https://github.com/guettli/programming-guidelines
+
--- djntra/models.py
+++ djntra/models.py
@@ -1,3 +1,22 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Thing(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=1024, blank=True, default='')
+    text = models.TextField(blank=True)
+
+
+class RelationType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=1024, blank=True, default='')
+    text = models.TextField(blank=True)
+
+
+class Relation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name='relations')
+    other = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name='reverse_relations')
+    type = models.ForeignKey(RelationType, on_delete=models.CASCADE)
