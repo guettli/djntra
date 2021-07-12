cebb04a01fac67066de3002e53a5eaa1272b4bcc
Thomas Guettler <guettliml@thomas-guettler.de>
2021-07-12 09:08:56 +0200

    models changed: text to html and other stuff.

--- djntra/models.py
+++ djntra/models.py
@@ -6,17 +6,16 @@ from django.db import models
 class Thing(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=1024, blank=True, default='')
-    text = models.TextField(blank=True)
-
+    html = models.TextField(blank=True)
+    is_relation = models.BooleanField(default=False)
 
 class RelationType(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=1024, blank=True, default='')
-    text = models.TextField(blank=True)
-
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=1024, unique=True)
 
 class Relation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     thing = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name='relations')
     other = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name='reverse_relations')
-    type = models.ForeignKey(RelationType, on_delete=models.CASCADE)
+    type = models.ForeignKey(Thing, on_delete=models.CASCADE, related_name='relation_types')
